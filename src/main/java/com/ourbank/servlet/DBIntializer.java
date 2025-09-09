40ae3a6787ebbaa9663ad61d5c32b590958b048c
Pratik Jadhav <jadhavpratik20001@gmail.com>
2025-09-10 00:37:56 +0530

    Update DBIntializer.java

--- src/main/java/com/ourbank/servlet/DBIntializer.java
+++ src/main/java/com/ourbank/servlet/DBIntializer.java
@@ -4,6 +4,6 @@ public interface DBIntializer {
     String DRIVER = "com.mysql.cj.jdbc.Driver";
     String CON_STRING = "jdbc:mysql://localhost:3306/ourbank?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     String USERNAME = "root";
-    String PASSWORD = "MySQL@11";
+    String PASSWORD = "";
 }
 // String CON_STRING = "jdbc:mysql://localhost:3306/ourbank";
