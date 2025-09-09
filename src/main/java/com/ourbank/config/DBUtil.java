3447c0386bb1ee756bcdffabc86679684ea2c5ff
Pratik Jadhav <jadhavpratik20001@gmail.com>
2025-09-10 00:36:47 +0530

    Update DBUtil.java

--- src/main/java/com/ourbank/config/DBUtil.java
+++ src/main/java/com/ourbank/config/DBUtil.java
@@ -9,7 +9,7 @@ public class DBUtil {
     // Database credentials
     private static final String URL = "jdbc:mysql://localhost:3306/ourbank"; // change DB name if needed
     private static final String USER = "root"; // your MySQL username
-    private static final String PASSWORD = "MySQL@11"; // your MySQL password
+    private static final String PASSWORD = ""; // your MySQL password
 
     // Load MySQL driver only once (static block)
     static {
